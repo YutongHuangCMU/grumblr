11219c59dc0a92d5afa20f99a1051515915415b6
evelynhuangyt <evelynhuangyt@163.com>
2016-11-11 18:42:15 -0500

    a

--- webapps/settings.py
+++ webapps/settings.py
@@ -130,9 +130,18 @@ MEDIA_ROOT = PROJECT_ROOT + '/grumblr/media/'
 
 MEDIA_URL = '/media/'
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/grumblr/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+		os.path.join(PROJECT_ROOT, 'static'),
+		    )
