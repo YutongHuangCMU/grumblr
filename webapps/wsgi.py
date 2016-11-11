5d27ff822c232c389b59b9edc50e3a24637070b4
evelynhuangyt <evelynhuangyt@163.com>
2016-11-11 18:29:33 -0500

    b

--- webapps/wsgi.py
+++ webapps/wsgi.py
@@ -1,4 +1,5 @@
 """
+
 WSGI config for webapps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -7,7 +8,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-# import os
+import os
 #
 # from django.core.wsgi import get_wsgi_application
 #
