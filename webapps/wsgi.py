f8e2b3608091006a9434c38d10ecc903a6728480
evelynhuangyt <evelynhuangyt@163.com>
2016-11-11 18:45:34 -0500

    a

--- webapps/wsgi.py
+++ webapps/wsgi.py
@@ -18,9 +18,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-import sys
-sys.path.append("../")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#import sys
+#sys.path.append("../")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
