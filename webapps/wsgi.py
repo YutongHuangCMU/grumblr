7af50c5a1c2e13dca9ddc9539405c80782183e72
evelynhuangyt <evelynhuangyt@163.com>
2016-11-11 19:20:01 -0500

    wsgi

--- webapps/wsgi.py
+++ webapps/wsgi.py
@@ -20,7 +20,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 #import sys
 #sys.path.append("../")
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapps.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
